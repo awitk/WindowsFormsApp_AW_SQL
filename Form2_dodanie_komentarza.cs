94f4cacdf3435fde58fd354ffcdfd3574467ddfa
awi1988 <awi1988@gmail.com>
2022-08-27 21:51:50 +0200

    27.08

--- Form2_dodanie_komentarza.cs
+++ Form2_dodanie_komentarza.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using WindowsFormsApp_AW_SQL.Models2;
 
 namespace WindowsFormsApp_AW_SQL
 {
@@ -13,7 +15,37 @@ namespace WindowsFormsApp_AW_SQL
         public Form2_dodanie_komentarza()
         {
             InitializeComponent();
-        }
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=DESKTOP-D50UGU8\SQL_AW;Initial Catalog=master;Integrated Security=True";
+            System.Console.WriteLine(0);
+            con.Open();
+            var cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "select Komentarz_Id, Wynik_Id, Data_dodania,Komentarz_tresc, Komentarz_autor FROM KOMENTARZ";
+            System.Console.WriteLine(1);
+            var reader = cmd.ExecuteReader();
+
+
+            var list = new List<Wynik_F2>();
+            while (reader.Read())
+            {
+                System.Console.WriteLine(reader.ToString());
+                var w = new Wynik_F2
+
+                {
+                    KOMENTARZ_ID = (int)reader["Komentarz_Id"],
+                    WYNIK_ID = (int)reader["Wynik_Id"],
+                    Data_Dodania = reader["Data_dodania"].ToString(),
+                    Komentarz_tresc = reader["Komentarz_tresc"].ToString(),
+                    Komentarz_autor = reader["Komentarz_autor"].ToString()
+
+                };
+                list.Add(w);
+            }
+            dataGridView2.DataSource = list;
+                    }
+
 
         private void button_form2_usun_rekord_Click(object sender, EventArgs e)
         {
---
+++ Form_wyniki_jednej_edycji.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_AW_SQL
+{
+    public partial class Form_wyniki_jednej_edycji : Form
+    {
+        public Form_wyniki_jednej_edycji()
+        {
+            InitializeComponent();
+        }
+    }
+  }
